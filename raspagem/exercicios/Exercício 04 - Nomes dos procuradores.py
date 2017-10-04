76a978476220176bd11a47442fbe87daf363ed36
Aishameriane Schmidt <aishameriane@gmail.com>
2017-10-03 22:13:10 -0300

    Create Exercício 04 - Nomes dos procuradores.py

---
+++ raspagem/exercicios/Exercício 04 - Nomes dos procuradores.py
@@ -0,0 +1,12 @@
+# Pega o nome de todos os procuradores do Estado de São Paulo
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen("https://sismpconsultapublica.mpsp.mp.br/ConsultarDistribuicao/ObterFiltrosPorMembro")
+bsObj = BeautifulSoup(html.read(), "html.parser")
+nomes = bsObj.findAll("option")
+nomes = nomes[1:-5]
+
+for nome in nomes:
+    print(nome.getText())
